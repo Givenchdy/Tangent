aa490afe498b4e6dff336021ac1ee5e3042d1758
Givenchdy <givenchdy@icloud.com>
2018-06-10 21:47:01 +0200

    Added Api Adapter class to handle all the api requests

--- CloudSdk/CloudSdk/ApiInterface/ApiAdapter.cs
+++ CloudSdk/CloudSdk/ApiInterface/ApiAdapter.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CloudSdk.Model;
+using CloudSdk.Models;
+using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +11,27 @@ namespace CloudSdk.ApiInterface
 {
     class ApiAdapter
     {
+
+        public EmployeeCollection FetchEmployees()
+        {
+
+            var client = new RestClient(ApiSettings.BASE_URL);       
+            var request = new RestRequest(ApiSettings.GET_EMPLOYEES_URL, Method.GET);
+            request.AddParameter("Authorisation", ApiSettings.AUTH_TOKEN);
+            IRestResponse response = client.Execute(request);
+
+            return null;
+        }
+
+        public User FetchMyProfile(string userID)
+        {
+            var client = new RestClient(ApiSettings.BASE_URL);
+            var request = new RestRequest(ApiSettings.GET_MY_PROFILE, Method.GET);
+            request.AddParameter("Authorisation", ApiSettings.AUTH_TOKEN);
+            IRestResponse response = client.Execute(request);
+
+            return null;
+        }
 
     }
 }

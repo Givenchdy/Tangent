ba07df8eaacbfffa5c95961b74aa3bb7586f408e
Givenchdy <givenchdy@icloud.com>
2018-06-10 21:47:26 +0200

    Added the api settings class to store all the api config info

--- CloudSdk/CloudSdk/ApiInterface/ApiSettings.cs
+++ CloudSdk/CloudSdk/ApiInterface/ApiSettings.cs
@@ -9,7 +9,7 @@ namespace CloudSdk.ApiInterface
     class ApiSettings
     {
 
-        public const string AUTH_TOKEN = "";
+        public const string AUTH_TOKEN = "Token 2a3d1af2f3f6d1cddaa3012c1c465fcbdffa3678";
         public const string BASE_URL = "http://staging.tangent.tngnt.co/api/";
         public const string GET_EMPLOYEES_URL = "employee/";
         public const string GET_MY_PROFILE = "employee/me/";

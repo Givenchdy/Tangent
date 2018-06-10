9aded0552c2e74117b773465c5695a5e59ebe225
Givenchdy <givenchdy@icloud.com>
2018-06-10 20:09:51 +0200

    Initial commit of the CloudSdk library

---
+++ CloudSdk/CloudSdk/ApiInterface/ApiSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSdk.ApiInterface
+{
+    class ApiSettings
+    {
+
+        public const string AUTH_TOKEN = "";
+        public const string BASE_URL = "http://staging.tangent.tngnt.co/api/";
+        public const string GET_EMPLOYEES_URL = "employee/";
+        public const string GET_MY_PROFILE = "employee/me/";
+
+    }
+}

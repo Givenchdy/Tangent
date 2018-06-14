c3f190a8a5777fc03b802b31c90c2e45793bf83d
genioProRoast <proroast@genioroasters.co.za>
2018-06-15 00:23:35 +0200

    Added review type enum

---
+++ CloudSdk/CloudSdk/Enums/ReviewType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSdk.Enums
+{
+    class ReviewType
+    {
+
+        public const string P = "Performance Increase";
+        public const string S = "Starting Salary";
+        public const string A = "Annual Increase";
+        public const string E = "Expectation Review";
+
+    }
+}

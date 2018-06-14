7c72aa1dcc13316fab2769ed952e51091fa7fb13
genioProRoast <proroast@genioroasters.co.za>
2018-06-15 00:23:12 +0200

    Added next of kin view model for passing data to the ui

---
+++ PCL/PCL/ViewModel/NextOfKinViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PCL.ViewModel
+{
+    public class NextOfKinViewModel
+    {
+
+
+        public int ID
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Relationship
+        {
+            get;
+            set;
+        }
+
+        public string PhoneNumber
+        {
+            get;
+            set;
+        }
+
+        public string Email
+        {
+            get;
+            set;
+        }
+
+        public string PhysicalAddress
+        {
+            get;
+            set;
+        }
+
+
+        public int EmployeeID
+        {
+            get;
+            set;
+        }
+
+
+    }
+}

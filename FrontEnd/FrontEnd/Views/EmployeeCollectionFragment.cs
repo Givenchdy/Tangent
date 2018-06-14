5878726bd8e3fa306d6ee1778ac1b70a1d9ec15c
genioProRoast <proroast@genioroasters.co.za>
2018-06-14 22:43:45 +0200

    Added logic to set tag in activity class

--- FrontEnd/FrontEnd/Views/EmployeeCollectionFragment.cs
+++ FrontEnd/FrontEnd/Views/EmployeeCollectionFragment.cs
@@ -19,17 +19,28 @@ namespace FrontEnd.Views
     public class EmployeeCollectionFragment : Fragment
     {
 
+        public static string TAG = "EmployeeCollectionFragment";
         [BindView(Resource.Id.employeeListID)] ListView employeeListView;
         EmployeeListItemAdapter employeeListItemAdapter;
         LinkedList<EmployeeViewModel> employeeList;
 
         public FilterLookup filter;
+        public string filterValue;
 
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             employeeList = DashBoardView.dashBoardPresenter.FilteredemployeeList;
+            DashBoardView.CurrentFragmentTag = TAG;
+            DashBoardView.exitCounter = 0;
+
+            if(filterValue != null)
+            {
+                DashBoardView.dashBoardPresenter.GetFilteredEmployeeCollection(filter, filterValue);
+                filterValue = null;
+            }
+
 
             // Create your fragment here
         }
@@ -40,16 +51,29 @@ namespace FrontEnd.Views
             View  view =  inflater.Inflate(Resource.Layout.employee_collection_fragment_view, container, false);
             Cheeseknife.Bind(this, view);
             employeeListView.ItemClick += listClick;
-        
-            if (employeeList != null)
-            {
-                EmployeeListItemAdapter employeeListItemAdapter = new EmployeeListItemAdapter(Context, employeeList);
-                employeeListView.Adapter = employeeListItemAdapter;
-            }
+
+            PopulateList();
 
             return view;
         }
 
+        private void PopulateList()
+        {
+
+            if (employeeList != null)
+            {
+                Log.Debug(TAG, "Populate List View: " + employeeList.Count);
+                EmployeeListItemAdapter employeeListItemAdapter = new EmployeeListItemAdapter(Context, employeeList);
+                employeeListView.Adapter = employeeListItemAdapter;
+            }
+            else
+            {
+                DashBoardView.dashBoardPresenter.InitializeCollection();
+                employeeList = DashBoardView.dashBoardPresenter.FilteredemployeeList;
+                PopulateList();
+            }
+
+        }
 
         private void listClick(object sender, AdapterView.ItemClickEventArgs e)
         {

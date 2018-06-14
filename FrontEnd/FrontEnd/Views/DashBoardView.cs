d9ff94fdb8d6452c4eb7ce14e3a07d5b07b34838
genioProRoast <proroast@genioroasters.co.za>
2018-06-14 22:41:46 +0200

    Added logic for handling different fragments

--- FrontEnd/FrontEnd/Views/DashBoardView.cs
+++ FrontEnd/FrontEnd/Views/DashBoardView.cs
@@ -20,12 +20,16 @@ namespace FrontEnd.Views
     public class DashBoardView : Activity
     {
 
+        public static string CurrentFragmentTag = DashboardSummaryFragment.TAG;
         public static DashBoardPresenter dashBoardPresenter;
+        public static Context context;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.dashboard_view);
+
+            context = this;
 
             dashBoardPresenter = new DashBoardPresenter();
             dashBoardPresenter.InitializeCollection();
@@ -34,12 +38,46 @@ namespace FrontEnd.Views
             // Create your application here
         }
 
+        public static int exitCounter = 0;
+        public override void OnBackPressed()
+        {
+            //base.OnBackPressed();
+            if (CurrentFragmentTag.Equals(DashboardSummaryFragment.TAG))
+            {
+                if (exitCounter == 0)
+                {
+                    exitCounter++;
+                    Toast.MakeText(this, "Press again to exit app", ToastLength.Short).Show();
+                }
+                else
+                {
+                    exitCounter = 0;
+                    base.OnBackPressed();
+                }
+            
+            }
+            else if(CurrentFragmentTag.Equals(EmployeeCollectionFragment.TAG))
+            {
+                ShowDashBoardSummaryFragment();
+            }
+            else if(CurrentFragmentTag.Equals(UserProfileFragment.TAG))
+            {
+                EmployeeCollectionFragment employeeCollectionFragment = new EmployeeCollectionFragment();
+                ShowFragment(employeeCollectionFragment);
+            }
+        }
+
         private void ShowDashBoardSummaryFragment()
         {
-
             DashboardSummaryFragment dashboardFragmentViewFragment = new DashboardSummaryFragment();
+
+            ShowFragment(dashboardFragmentViewFragment);
+        }
+
+        private void ShowFragment(Fragment fragment)
+        {
             FragmentTransaction transaction = FragmentManager.BeginTransaction();
-            transaction.Replace(Resource.Id.fragmentContainerID, dashboardFragmentViewFragment);
+            transaction.Replace(Resource.Id.fragmentContainerID, fragment);
             transaction.Commit();
         }
 

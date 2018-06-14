4fbb31ccb7c7fc68f3485dc551969d64afaee32f
genioProRoast <proroast@genioroasters.co.za>
2018-06-14 22:40:09 +0200

    Added position model for employee position in the company

---
+++ CloudSdk/CloudSdk/Models/Position.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSdk.Models
+{
+
+    [Newtonsoft.Json.JsonObject(Title = "position")]
+    public class Position
+    {
+        public int id;
+        public string name;
+        public string level;
+        public int sort;
+
+    }
+}

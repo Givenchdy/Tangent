181f93e7e633c956c10bbdfa29b7df827cb99708
genioProRoast <proroast@genioroasters.co.za>
2018-06-15 18:03:48 +0200

    Added review view model for displaying purpose

---
+++ PCL/PCL/ViewModel/ReviewViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PCL.ViewModel
+{
+    public class ReviewViewModel
+    {
+
+        public int ID;
+        public string Date;
+        public string Salary;
+        public string Type;
+
+    }
+}

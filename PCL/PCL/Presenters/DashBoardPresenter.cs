bcda5e282f552454d9444194cccf93c3e8ef292b
genioProRoast <proroast@genioroasters.co.za>
2018-06-15 18:04:29 +0200

    Created aa method for fetching my profile data

--- PCL/PCL/Presenters/DashBoardPresenter.cs
+++ PCL/PCL/Presenters/DashBoardPresenter.cs
@@ -29,6 +29,12 @@ namespace PCL.Presenters
 
         }
 
+        public EmployeeViewModel FetchMyProfile()
+        {
+           EmployeeViewModel employeeViewModel =  EmployeeViewModel.FetchMyProfile();
+            return employeeViewModel;
+        }
+
         private string formFilterQueryString(FilterLookup filter, string filterValue)
         {
 
@@ -41,46 +47,6 @@ namespace PCL.Presenters
                 default:
                     return null;
             }
-
-        }
-
-
-        private void PopulateEmployeeCollectionWithTestData()
-        {
-
-            EmployeeViewModel employeeViewModel1 = new EmployeeViewModel();
-            employeeViewModel1.EmployeeID = 2;
-            employeeViewModel1.FirstName = "Lerato";
-            employeeViewModel1.LastName = "Malatji";
-            employeeViewModel1.PositionName = "Sales Lead";
-            employeeViewModel1.EmployeeLevel = "Senior";
-
-            EmployeeViewModel employeeViewModel2 = new EmployeeViewModel();
-            employeeViewModel2.EmployeeID = 13;
-            employeeViewModel2.FirstName = "keabetswe";
-            employeeViewModel2.LastName = "Motupa";
-            employeeViewModel2.PositionName = "Developer";
-            employeeViewModel2.EmployeeLevel = "Junior";
-
-            EmployeeViewModel employeeViewModel3 = new EmployeeViewModel();
-            employeeViewModel3.EmployeeID = 13;
-            employeeViewModel3.FirstName = "Masefako";
-            employeeViewModel3.LastName = "Mojapelo";
-            employeeViewModel3.PositionName = "Developer";
-            employeeViewModel3.EmployeeLevel = "Intermediate";
-
-            EmployeeViewModel employeeViewModel4 = new EmployeeViewModel();
-            employeeViewModel4.EmployeeID = 13;
-            employeeViewModel4.FirstName = "Mothibi";
-            employeeViewModel4.LastName = "Mojapelo";
-            employeeViewModel4.PositionName = "Sales Person";
-            employeeViewModel4.EmployeeLevel = "Intermediate";
-
-            FilteredemployeeList.AddLast(employeeViewModel4);
-            FilteredemployeeList.AddLast(employeeViewModel1);
-            FilteredemployeeList.AddLast(employeeViewModel2);
-            FilteredemployeeList.AddLast(employeeViewModel3);
-
 
         }
 

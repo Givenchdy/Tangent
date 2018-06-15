2df321b90b872946d354862273e39b7e2c4497a3
genioProRoast <proroast@genioroasters.co.za>
2018-06-15 18:03:17 +0200

    Added next of kin collection model

---
+++ CloudSdk/CloudSdk/Models/NextOfKinList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSdk.Models
+{
+
+    [Newtonsoft.Json.JsonObject(Title = "employee_next_of_kin")]
+    class NextOfKinList
+    {
+        public LinkedList<NextOfKin> employee_next_of_kin = new LinkedList<NextOfKin>();
+    }
+}
